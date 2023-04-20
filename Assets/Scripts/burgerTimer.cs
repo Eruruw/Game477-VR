d29862de660d0c3b55910bc67d4658041325fa5a
Ty Pederson <62142901+TyPe23@users.noreply.github.com>
2023-04-20 01:17:52 -0500

    small tutorial stuff

--- Assets/Scripts/burgerTimer.cs
+++ Assets/Scripts/burgerTimer.cs
@@ -58,7 +58,6 @@ public class burgerTimer : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("tray"))
         {
-            cooking = true;
             if (tutorial.currentStep == 7)
             {
                 tutorial.incrementStep(7);
